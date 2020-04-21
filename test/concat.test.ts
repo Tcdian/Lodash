407505697d59ef47a95ab3b0be51d6a9564f8090
tcdian <tcdiank@gmail.com>
2020-04-21 21:32:50 +0800

    :sparkles: concat

---
+++ test/concat.test.ts
@@ -0,0 +1,6 @@
+import _ from 'lodash';
+import concat from '../source/concat';
+
+test(`concat(array, 2, [3]) => ${_.concat([1], 2, [3])}`, () => {
+    expect(concat([1], 2, [3])).toEqual(_.concat([1], 2, [3]));
+});
