7ee896e0507e07e0ab1d093fbe0d57024763fa45
tcdian <tcdiank@gmail.com>
2020-04-23 20:23:20 +0800

    :sparkles: join

---
+++ source/join.ts
@@ -0,0 +1,5 @@
+function join<T>(array: T[], separator: string = ','): string {
+    return array.join(separator);
+}
+
+export default join;
---
+++ test/join.test.ts
@@ -0,0 +1,6 @@
+import _ from 'lodash';
+import join from '../source/join';
+
+test(`join(['a', 'b', 'c'], '~') => ${_.join(['a', 'b', 'c'], '~')}`, () => {
+    expect(join(['a', 'b', 'c'], '~')).toBe(_.join(['a', 'b', 'c'], '~'));
+});
