808492c2479067372bb9fe9b2308ff6ef3cfd277
tcdian <tcdiank@gmail.com>
2020-04-24 20:43:44 +0800

    :sparkles: pullAt

---
+++ test/pullAt.test.ts
@@ -0,0 +1,10 @@
+import _ from 'lodash';
+import pullAt from '../source/pullAt';
+
+const arr1 = ['a', 'b', 'c', 'd'];
+const arr2 = ['a', 'b', 'c', 'd'];
+
+test(`pullAt(['a', 'b', 'c', 'd'], [1, 3]) => ${_.pullAt(['a', 'b', 'c', 'd'], [1, 3])}`, () => {
+    expect(_.pullAt(arr1, [1, 3])).toEqual(pullAt(arr2, [1, 3]));
+    expect(arr1).toEqual(arr2);
+});
