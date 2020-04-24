d650dbb4c01ea5a35f13125e4508c075eccde5e5
tcdian <tcdiank@gmail.com>
2020-04-24 20:50:23 +0800

    :sparkles: reverse

---
+++ test/reverse.test.ts
@@ -0,0 +1,10 @@
+import _ from 'lodash';
+import reverse from '../source/reverse';
+
+const arr1 = [1, 2, 3];
+const arr2 = [1, 2, 3];
+
+test(`reverse([1, 2, 3]) => ${reverse([1, 2, 3])}`, () => {
+    expect(reverse(arr1)).toEqual(_.reverse(arr2));
+    expect(arr1).toEqual(arr2);
+});
