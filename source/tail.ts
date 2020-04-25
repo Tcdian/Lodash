3fd51971f5a5a4d4f6ed87753ce9b5e372349279
tcdian <tcdiank@gmail.com>
2020-04-25 11:42:01 +0800

    :sparkles: tail

---
+++ source/tail.ts
@@ -0,0 +1,5 @@
+function tail<T>(array: T[]): T[] {
+    return array.slice(1);
+}
+
+export default tail;
---
+++ test/tail.test.ts
@@ -0,0 +1,6 @@
+import _ from 'lodash';
+import tail from '../source/tail';
+
+test(`tail([1, 2, 3]) => ${tail([1, 2, 3])}`, () => {
+    expect(tail([1, 2, 3])).toEqual(_.tail([1, 2, 3]));
+});
