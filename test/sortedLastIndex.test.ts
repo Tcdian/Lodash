318bb287c99706bd8671886d4826a079f673ce41
tcdian <tcdiank@gmail.com>
2020-04-25 11:27:55 +0800

    :sparkles: sortedLastIndex

---
+++ test/sortedLastIndex.test.ts
@@ -0,0 +1,14 @@
+import _ from 'lodash';
+import sortedLastIndex from '../source/sortedLastIndex';
+
+test(`sortedLastIndex([30, 50], 40) => ${sortedLastIndex([30, 50], 40)}`, () => {
+    expect(sortedLastIndex([30, 50], 40)).toBe(_.sortedLastIndex([30, 50], 40));
+});
+
+test(`sortedLastIndex([30, 50], 60) => ${sortedLastIndex([30, 50], 60)}`, () => {
+    expect(sortedLastIndex([30, 50], 60)).toBe(_.sortedLastIndex([30, 50], 60));
+});
+
+test(`sortedLastIndex([30, 50], 10) => ${sortedLastIndex([30, 50], 10)}`, () => {
+    expect(sortedLastIndex([30, 50], 10)).toBe(_.sortedLastIndex([30, 50], 10));
+});
