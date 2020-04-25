eb5fe83a7c453f4959f441ec70c719eb38fb9b96
tcdian <tcdiank@gmail.com>
2020-04-25 15:03:55 +0800

    :sparkles: xor

---
+++ test/xor.test.ts
@@ -0,0 +1,14 @@
+import _ from 'lodash';
+import xor from '../source/xor';
+
+test(`xor([2, 1], [2, 3]) => ${xor([2, 1], [2, 3])}`, () => {
+    expect(xor([2, 1], [2, 3])).toEqual(_.xor([2, 1], [2, 3]));
+});
+
+test(`xor([1, 2, 1], [2, 3]) => ${xor([1, 2, 1], [2, 3])}`, () => {
+    expect(xor([1, 2, 1], [2, 3])).toEqual(_.xor([1, 2, 1], [2, 3]));
+});
+
+test(`xor([], [2, 1], [2, 3]) => ${xor([], [2, 1], [2, 3])}`, () => {
+    expect(xor([], [2, 1], [2, 3])).toEqual(_.xor([], [2, 1], [2, 3]));
+});
