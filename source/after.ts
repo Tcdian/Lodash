54977c974c4ebb7b76a46cdf1c693e4ada9ca17a
tcdian <tcdiank@gmail.com>
2020-04-26 20:34:49 +0800

    :label: fixed after type

--- source/after.ts
+++ source/after.ts
@@ -1,5 +1,6 @@
 type Func = (...args: any[]) => any;
 
+function after<TFunc extends Func>(n: number, func: TFunc): TFunc;
 function after(n: number, func: Func): Func {
     return function (this: any, ...args: any[]): any {
         if (--n <= 0) {
