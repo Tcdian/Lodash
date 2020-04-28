cad1c65070f795ad441970b96b873d4bf8d5a74c
tcdian <tcdiank@gmail.com>
2020-04-28 21:21:20 +0800

    :sparkles: memoize

---
+++ test/memoize.test.ts
@@ -0,0 +1,43 @@
+import memoize from '../source/memoize';
+
+test('memoize 函数测试', () => {
+    const mockFn = jest.fn((value: string) => value);
+    const memoized = memoize(mockFn);
+
+    const first = memoized('first');
+    expect(first).toBe('first');
+    expect(mockFn).toBeCalledTimes(1);
+    memoized('first');
+    expect(mockFn).toBeCalledTimes(1);
+
+    const second = memoized('second');
+    expect(second).toBe('second');
+    expect(mockFn).toBeCalledTimes(2);
+    expect(memoized.cache).toEqual(
+        new Map([
+            ['first', 'first'],
+            ['second', 'second'],
+        ])
+    );
+});
+
+test('memoize 函数测试', () => {
+    const mockFn = jest.fn((value: string) => value);
+    const memoized = memoize(mockFn, (key: string) => `key_${key}`);
+
+    const first = memoized('first');
+    expect(first).toBe('first');
+    expect(mockFn).toBeCalledTimes(1);
+    memoized('first');
+    expect(mockFn).toBeCalledTimes(1);
+
+    const second = memoized('second');
+    expect(second).toBe('second');
+    expect(mockFn).toBeCalledTimes(2);
+    expect(memoized.cache).toEqual(
+        new Map([
+            ['key_first', 'first'],
+            ['key_second', 'second'],
+        ])
+    );
+});
