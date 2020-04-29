56ca6ecea79f1dff199621e746efe4047aa78f56
tcdian <tcdiank@gmail.com>
2020-04-29 20:59:06 +0800

    :label: update negate type

--- source/negate.ts
+++ source/negate.ts
@@ -1,6 +1,6 @@
 function negate<T extends (...args: any[]) => boolean>(predicate: T): T;
 function negate(predicate: (...args: any[]) => boolean): (...args: any[]) => boolean {
-    return function (this: any, ...args: any[]) {
+    return function (this: any, ...args: any[]): boolean {
         return !predicate.call(this, ...args);
     };
 }
