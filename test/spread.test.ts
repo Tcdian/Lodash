07a11b9818455151397d4cc7bc2d9ca928abe5a6
tcdian <tcdiank@gmail.com>
2020-04-29 21:32:26 +0800

    :sparkles: spread

---
+++ test/spread.test.ts
@@ -0,0 +1,19 @@
+import spread from '../source/spread';
+
+test('spread 函数测试1', () => {
+    const say = spread(function (who, what) {
+        return who + ' says ' + what;
+    });
+
+    expect(say(['fred', 'hello'])).toBe('fred says hello');
+});
+
+test('spread 函数测试2', async () => {
+    const numbers = Promise.all([Promise.resolve(40), Promise.resolve(18)]);
+    const sum = await numbers.then(
+        spread<number>(function (x, y) {
+            return x + y;
+        })
+    );
+    expect(sum).toBe(58);
+});
