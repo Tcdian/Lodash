3dfba4e26a34c17ad59879d7b342a7c2f78f3909
tcdian <tcdiank@gmail.com>
2020-05-01 17:26:50 +0800

    :sparkles: castArray

---
+++ test/castArray.test.ts
@@ -0,0 +1,31 @@
+import _ from 'lodash';
+import castArray from '../source/castArray';
+
+test(`castArray(1) => ${castArray(1)}`, () => {
+    expect(castArray(1)).toEqual(_.castArray(1));
+});
+
+test(`castArray({ 'a': 1 }) => ${castArray({ a: 1 })}`, () => {
+    expect(castArray({ a: 1 })).toEqual(_.castArray({ a: 1 }));
+});
+
+test(`castArray('abc') => ${castArray('abc')}`, () => {
+    expect(castArray('abc')).toEqual(_.castArray('abc'));
+});
+
+test(`castArray(null) => ${castArray(null)}`, () => {
+    expect(castArray(null)).toEqual(_.castArray(null));
+});
+
+test(`castArray(undefined) =>${castArray(undefined)}`, () => {
+    expect(castArray(undefined)).toEqual(_.castArray(undefined));
+});
+
+test(`castArray() => ${castArray()}`, () => {
+    expect(castArray()).toEqual(_.castArray());
+});
+
+test(`castArray([1, 2, 3]) => ${castArray([1, 2, 3])}`, () => {
+    const arr = [1, 2, 3];
+    expect(castArray(arr)).toBe(arr);
+});
