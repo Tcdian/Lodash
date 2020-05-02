860296d26c516c28508b0ea8503024ba3e0ff9fd
tcdian <tcdiank@gmail.com>
2020-05-02 11:35:32 +0800

    :sparkles: isArray

---
+++ test/isArray.test.ts
@@ -0,0 +1,10 @@
+import _ from 'lodash';
+import isArray from '../source/isArray';
+
+test(`isArray([1, 2, 3]) => ${isArray([1, 2, 3])}`, () => {
+    expect(isArray([1, 2, 3])).toBe(_.isArray([1, 2, 3]));
+});
+
+test(`isArray('abc') => ${isArray('abc')}`, () => {
+    expect(isArray('abc')).toBe(_.isArray('abc'));
+});
