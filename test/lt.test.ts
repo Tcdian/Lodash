29f48861bde795384284de7db6226d41002bd44c
tcdian <tcdiank@gmail.com>
2020-05-02 17:12:37 +0800

    :sparkles: lt

---
+++ test/lt.test.ts
@@ -0,0 +1,14 @@
+import _ from 'lodash';
+import lt from '../source/lt';
+
+test(`lt(1, 3) => ${lt(1, 3)}`, () => {
+    expect(lt(1, 3)).toBe(_.lt(1, 3));
+});
+
+test(`lt(3, 3) => ${lt(3, 3)}`, () => {
+    expect(lt(3, 3)).toBe(_.lt(3, 3));
+});
+
+test(`lt(3, 1) => ${lt(3, 1)}`, () => {
+    expect(lt(3, 1)).toBe(_.lt(3, 1));
+});
