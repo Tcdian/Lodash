1334bc550f0fb95ee831263cb13e09d01ca8fd11
tcdian <tcdiank@gmail.com>
2020-05-02 14:46:34 +0800

    :sparkles: isNull

---
+++ test/isNull.test.ts
@@ -0,0 +1,10 @@
+import _ from 'lodash';
+import isNull from '../source/isNull';
+
+test(`isNull(null) => ${isNull(null)}`, () => {
+    expect(isNull(null)).toBe(_.isNull(null));
+});
+
+test(`isNull(void 0) => ${isNull(void 0)}`, () => {
+    expect(isNull(void 0)).toBe(_.isNull(void 0));
+});
