525819c1848e62e728f7f8f477027deb34cb9efd
tcdian <tcdiank@gmail.com>
2020-05-02 13:53:20 +0800

    :sparkles: isError

---
+++ test/isError.test.ts
@@ -0,0 +1,10 @@
+import _ from 'lodash';
+import isError from '../source/isError';
+
+test(`isError(new Error) => ${isError(new Error())}`, () => {
+    expect(isError(new Error())).toBe(_.isError(new Error()));
+});
+
+test(`isError(Error) => ${isError(Error)}`, () => {
+    expect(isError(Error)).toBe(_.isError(Error));
+});
