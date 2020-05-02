c4a7d73b10b2c7fc1ec9025e51956b2dba4d7660
tcdian <tcdiank@gmail.com>
2020-05-02 16:57:13 +0800

    :sparkles: isEmpty

---
+++ test/isEmpty.test.ts
@@ -0,0 +1,22 @@
+import _ from 'lodash';
+import isEmpty from '../source/isEmpty';
+
+test(`isEmpty(null) => ${isEmpty(null)}`, () => {
+    expect(isEmpty(null)).toBe(_.isEmpty(null));
+});
+
+test(`isEmpty(true) => ${isEmpty(true)}`, () => {
+    expect(isEmpty(true)).toBe(_.isEmpty(true));
+});
+
+test(`isEmpty(1) => ${isEmpty(1)}`, () => {
+    expect(isEmpty(1)).toBe(_.isEmpty(1));
+});
+
+test(`isEmpty([1, 2, 3]) => ${isEmpty([1, 2, 3])}`, () => {
+    expect(isEmpty([1, 2, 3])).toBe(_.isEmpty([1, 2, 3]));
+});
+
+test(`isEmpty({ 'a': 1 }) => ${isEmpty({ a: 1 })}`, () => {
+    expect(isEmpty({ a: 1 })).toBe(_.isEmpty({ a: 1 }));
+});
