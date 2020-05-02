b5789b3d55a586af6684452d1b9aa655c16a3bb6
tcdian <tcdiank@gmail.com>
2020-05-02 11:13:04 +0800

    :sparkles: eq

---
+++ test/eq.test.ts
@@ -0,0 +1,18 @@
+import _ from 'lodash';
+import eq from '../source/eq';
+
+test(`eq('a', 'a') => ${eq('a', 'a')}`, () => {
+    expect(eq('a', 'a')).toBe(_.eq('a', 'a'));
+});
+
+test(`eq('a', Object('a')) => ${eq('a', Object('a'))}`, () => {
+    expect(eq('a', Object('a'))).toBe(_.eq('a', Object('a')));
+});
+
+test(`eq({ 'a': 1 }, { 'a': 1 }) => ${eq({ a: 1 }, { a: 1 })}`, () => {
+    expect(eq({ a: 1 }, { a: 1 })).toBe(_.eq({ a: 1 }, { a: 1 }));
+});
+
+test(`eq(NaN, NaN) => ${eq(NaN, NaN)}`, () => {
+    expect(eq(NaN, NaN)).toBe(_.eq(NaN, NaN));
+});
