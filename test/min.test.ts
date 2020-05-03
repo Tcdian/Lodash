956d5ff941105de5945daf93a5980000b440ab9d
tcdian <tcdiank@gmail.com>
2020-05-03 16:56:07 +0800

    :sparkles: min

---
+++ test/min.test.ts
@@ -0,0 +1,14 @@
+import _ from 'lodash';
+import min from '../source/min';
+
+test(`min([4, 2, 8, 6]) => ${min([4, 2, 8, 6])}`, () => {
+    expect(min([4, 2, 8, 6])).toBe(_.min([4, 2, 8, 6]));
+});
+
+test(`min(['a', 'b', 'c']) => ${min(['a', 'b', 'c'])}`, () => {
+    expect(min(['a', 'b', 'c'])).toBe(_.min(['a', 'b', 'c']));
+});
+
+test(`min([]) => ${min([])}`, () => {
+    expect(min([])).toBe(_.min([]));
+});
