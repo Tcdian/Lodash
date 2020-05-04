f419af6c166ce04a98e566d41384c792d39a07b3
tcdian <tcdiank@gmail.com>
2020-05-04 17:39:38 +0800

    :bug: fix toPath

--- test/toPath.test.ts
+++ test/toPath.test.ts
@@ -9,6 +9,6 @@ test(`toPath('a[0].b.c') => ${toPath('a[0].b.c')}`, () => {
     expect(toPath('a[0].b.c')).toEqual(_.toPath('a[0].b.c'));
 });
 
-test(`toPath('a[0][1].b[2].c') => ${toPath('a[0][1].b[2].c')}`, () => {
-    expect(toPath('a[0][1].b[2].c')).toEqual(_.toPath('a[0][1].b[2].c'));
+test(`toPath('a[0][1].b[2]') => ${toPath('a[0][1].b[2]')}`, () => {
+    expect(toPath('a[0][1].b[2]')).toEqual(_.toPath('a[0][1].b[2]'));
 });
