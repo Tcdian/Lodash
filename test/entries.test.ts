ce16f3d61abd284296a4b488f7788002d097dc17
tcdian <tcdiank@gmail.com>
2020-05-05 13:54:45 +0800

    :art: entries

--- test/entries.test.ts
+++ test/entries.test.ts
@@ -4,3 +4,11 @@ import entries from '../source/entries';
 test(`entries({ a: 1, b: 2 }) => ${entries({ a: 1, b: 2 })}`, () => {
     expect(entries({ a: 1, b: 2 })).toEqual(_.entries({ a: 1, b: 2 }));
 });
+
+test(`entries(new Set([1])) => ${entries(new Set([1]))}`, () => {
+    expect(entries(new Set([1]))).toEqual(_.entries(new Set([1])));
+});
+
+test(`entries(new Map([['a', 1]])) => ${entries(new Map([['a', 1]]))}`, () => {
+    expect(entries(new Map([['a', 1]]))).toEqual(_.entries(new Map([['a', 1]])));
+});
