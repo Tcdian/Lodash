880f4643866951129496d698d7a57269a422aa24
tcdian <tcdiank@gmail.com>
2020-05-05 14:21:10 +0800

    :label: update create type

--- source/create.ts
+++ source/create.ts
@@ -1,4 +1,6 @@
-function create<T extends object, U extends object>(prototype: T, properties?: U): T & U {
+function create<T extends object>(prototype: T): T;
+function create<T extends object, U extends object>(prototype: T, properties: U): T & U;
+function create(prototype: object, properties?: object): any {
     return Object.assign(Object.create(prototype), properties);
 }
 
