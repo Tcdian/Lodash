cc6869aca78070b2c1d8dfdcb2c66a6e55678f7a
tcdian <tcdiank@gmail.com>
2020-05-07 20:37:13 +0800

    :sparkles: endsWith

---
+++ source/endsWith.ts
@@ -0,0 +1,5 @@
+function endsWith(string: string, target: string, position: number = string.length): boolean {
+    return string.endsWith(target, position);
+}
+
+export default endsWith;
---
+++ test/endsWith.test.ts
@@ -0,0 +1,14 @@
+import _ from 'lodash';
+import endsWith from '../source/endsWith';
+
+test(`endsWith('abc', 'c') => ${endsWith('abc', 'c')}`, () => {
+    expect(endsWith('abc', 'c')).toBe(_.endsWith('abc', 'c'));
+});
+
+test(`endsWith('abc', 'b') => ${endsWith('abc', 'b')}`, () => {
+    expect(endsWith('abc', 'b')).toBe(_.endsWith('abc', 'b'));
+});
+
+test(`endsWith('abc', 'b', 2) => ${endsWith('abc', 'b', 2)}`, () => {
+    expect(endsWith('abc', 'b', 2)).toBe(_.endsWith('abc', 'b', 2));
+});
