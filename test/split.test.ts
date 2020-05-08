841cd774a7171c62d364ad841b3ef8a9c855ded2
tcdian <tcdiank@gmail.com>
2020-05-08 21:30:54 +0800

    :sparkles: split

---
+++ test/split.test.ts
@@ -0,0 +1,10 @@
+import _ from 'lodash';
+import split from '../source/split';
+
+test(`split('a-b-c', '-') => ${split('a-b-c', '-')}`, () => {
+    expect(split('a-b-c', '-')).toEqual(_.split('a-b-c', '-'));
+});
+
+test(`split('a-b-c', '-', 2) => ${split('a-b-c', '-', 2)}`, () => {
+    expect(split('a-b-c', '-', 2)).toEqual(_.split('a-b-c', '-', 2));
+});
