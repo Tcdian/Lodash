8ed4112cfacc4eea473f41f628035723f547eedf
tcdian <tcdiank@gmail.com>
2020-05-08 21:02:16 +0800

    :sparkles: pad

---
+++ test/pad.test.ts
@@ -0,0 +1,14 @@
+import _ from 'lodash';
+import pad from '../source/pad';
+
+test(`pad('abc', 8) => ${pad('abc', 8)}`, () => {
+    expect(pad('abc', 8)).toBe(_.pad('abc', 8));
+});
+
+test(`pad('abc', 8, '_-') => ${pad('abc', 8, '_-')}`, () => {
+    expect(pad('abc', 8, '_-')).toBe(_.pad('abc', 8, '_-'));
+});
+
+test(`pad('abc', 3) => ${pad('abc', 3)}`, () => {
+    expect(pad('abc', 3)).toBe(_.pad('abc', 3));
+});
