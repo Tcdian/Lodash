0c144ef9988d3d2ad25c6adf3a45743bf0fcf085
tcdian <tcdiank@gmail.com>
2020-05-08 21:35:26 +0800

    :sparkles: toLower

---
+++ test/toLower.test.ts
@@ -0,0 +1,14 @@
+import _ from 'lodash';
+import toLower from '../source/toLower';
+
+test(`toLower('--Foo-Bar--') => ${toLower('--Foo-Bar--')}`, () => {
+    expect(toLower('--Foo-Bar--')).toBe(_.toLower('--Foo-Bar--'));
+});
+
+test(`toLower('fooBar') => ${toLower('fooBar')}`, () => {
+    expect(toLower('fooBar')).toBe(_.toLower('fooBar'));
+});
+
+test(`toLower('__FOO_BAR__') => ${toLower('__FOO_BAR__')}`, () => {
+    expect(toLower('__FOO_BAR__')).toBe(_.toLower('__FOO_BAR__'));
+});
