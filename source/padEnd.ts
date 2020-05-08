ab39f38dead644807dac41219d16d8e2d9656373
tcdian <tcdiank@gmail.com>
2020-05-08 20:46:08 +0800

    :sparkles: padEnd

---
+++ source/padEnd.ts
@@ -0,0 +1,5 @@
+function padEnd(string: string, length: number, chars: string = ' '): string {
+    return string.padEnd(length, chars);
+}
+
+export default padEnd;
---
+++ test/padEnd.test.ts
@@ -0,0 +1,14 @@
+import _ from 'lodash';
+import padEnd from '../source/padEnd';
+
+test(`padEnd('abc', 6) => ${padEnd('abc', 6)}`, () => {
+    expect(padEnd('abc', 6)).toBe(_.padEnd('abc', 6));
+});
+
+test(`padEnd('abc', 6, '_-') => ${padEnd('abc', 6, '_-')}`, () => {
+    expect(padEnd('abc', 6, '_-')).toBe(_.padEnd('abc', 6, '_-'));
+});
+
+test(`padEnd('abc', 3) => ${padEnd('abc', 3)}`, () => {
+    expect(padEnd('abc', 3)).toBe(_.padEnd('abc', 3));
+});
