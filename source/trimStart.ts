154f91ddcb4e0095027c437bd357412a8b934a41
tcdian <tcdiank@gmail.com>
2020-05-09 20:08:23 +0800

    :art: trimStart

--- source/trimStart.ts
+++ source/trimStart.ts
@@ -1,13 +1,13 @@
 function trimStart(string: string, chars: string = ' '): string {
     const cache = new Set(chars);
-    let start = -1;
+    let start = 0;
     for (let i = 0; i < string.length; i++) {
         if (!cache.has(string[i])) {
             break;
         }
         start++;
     }
-    return string.slice(start + 1);
+    return string.slice(start);
 }
 
 export default trimStart;
