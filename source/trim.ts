8e108104ef28c5b61a5bf5ff549df250ffcf7b47
tcdian <tcdiank@gmail.com>
2020-05-09 20:23:14 +0800

    :sparkles: trim

---
+++ source/trim.ts
@@ -0,0 +1,11 @@
+import trimStart from './trimStart';
+import trimEnd from './trimEnd';
+
+function trim(string: string, chars?: string, guard?: any): string {
+    if (guard || chars === undefined) {
+        return string.trim();
+    }
+    return trimEnd(trimStart(string, chars), chars);
+}
+
+export default trim;
---
+++ test/trim.test.ts
@@ -0,0 +1,14 @@
+import _ from 'lodash';
+import trim from '../source/trim';
+
+test(`trim('  abc  ') => ${trim('  abc  ')}`, () => {
+    expect(trim('  abc  ')).toBe(_.trim('  abc  '));
+});
+
+test(`trim('-_-abc-_-', '_-') => ${trim('-_-abc-_-', '_-')}`, () => {
+    expect(trim('-_-abc-_-', '_-')).toBe(_.trim('-_-abc-_-', '_-'));
+});
+
+test(`_.map(['  foo  ', '  bar  '], _.trim) => ${_.map(['  foo  ', '  bar  '], trim)}`, () => {
+    expect(_.map(['  foo  ', '  bar  '], trim)).toEqual(_.map(['  foo  ', '  bar  '], _.trim));
+});
