c31e19167aecbc262861ba00a7ec55f3ed6d5773
tcdian <tcdiank@gmail.com>
2020-05-11 21:10:18 +0800

    :sparkles: clamp

---
+++ source/clamp.ts
@@ -0,0 +1,17 @@
+function clamp(number: number, lower: number, upper: number): number;
+function clamp(number: number, upper: number): number;
+function clamp(number: number, lower: number, upper?: number): number {
+    if (upper === undefined) {
+        upper = lower;
+        lower = 0;
+    }
+    if (number < lower) {
+        return lower;
+    }
+    if (number > upper) {
+        return upper;
+    }
+    return number;
+}
+
+export default clamp;
---
+++ test/clamp.test.ts
@@ -0,0 +1,14 @@
+import _ from 'lodash';
+import clamp from '../source/clamp';
+
+test(`clamp(-10, -5, 5) => ${clamp(-10, -5, 5)}`, () => {
+    expect(clamp(-10, -5, 5)).toBe(_.clamp(-10, -5, 5));
+});
+
+test(`clamp(10, -5, 5) => ${clamp(10, -5, 5)}`, () => {
+    expect(clamp(10, -5, 5)).toBe(_.clamp(10, -5, 5));
+});
+
+test(`clamp(0, -5, 5) => ${clamp(0, -5, 5)}`, () => {
+    expect(clamp(0, -5, 5)).toBe(_.clamp(0, -5, 5));
+});
