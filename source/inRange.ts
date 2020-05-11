eea3b682c700db5698d171bce0df19a82cab6bfb
tcdian <tcdiank@gmail.com>
2020-05-11 21:20:49 +0800

    :sparkles: inRange

---
+++ source/inRange.ts
@@ -0,0 +1,17 @@
+function inRange(number: number, start: number, end: number): boolean;
+function inRange(number: number, end: number): boolean;
+function inRange(number: number, start: number, end?: number): boolean {
+    if (end === undefined) {
+        end = start;
+        start = 0;
+    }
+    if (start > end) {
+        [start, end] = [end, start];
+    }
+    if (number >= start && number < end) {
+        return true;
+    }
+    return false;
+}
+
+export default inRange;
---
+++ test/inRange.test.ts
@@ -0,0 +1,30 @@
+import _ from 'lodash';
+import inRange from '../source/inRange';
+
+test(`inRange(3, 2, 4) => ${inRange(3, 2, 4)}`, () => {
+    expect(inRange(3, 2, 4)).toBe(_.inRange(3, 2, 4));
+});
+
+test(`inRange(4, 8) => ${inRange(4, 8)}`, () => {
+    expect(inRange(4, 8)).toBe(_.inRange(4, 8));
+});
+
+test(`inRange(4, 2) => ${inRange(4, 2)}`, () => {
+    expect(inRange(4, 2)).toBe(_.inRange(4, 2));
+});
+
+test(`inRange(2, 2) => ${inRange(2, 2)}`, () => {
+    expect(inRange(2, 2)).toBe(_.inRange(2, 2));
+});
+
+test(`inRange(1.2, 2) => ${inRange(1.2, 2)}`, () => {
+    expect(inRange(1.2, 2)).toBe(_.inRange(1.2, 2));
+});
+
+test(`inRange(5.2, 4) => ${inRange(5.2, 4)}`, () => {
+    expect(inRange(5.2, 4)).toBe(_.inRange(5.2, 4));
+});
+
+test(`inRange(-3, -2, -6) => ${inRange(-3, -2, -6)}`, () => {
+    expect(inRange(-3, -2, -6)).toBe(_.inRange(-3, -2, -6));
+});
