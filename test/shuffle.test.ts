5546e3cb788b188476a39f14045edc0ced2e312f
tcdian <tcdiank@gmail.com>
2020-05-12 21:35:19 +0800

    :sparkles: shuffle

---
+++ test/shuffle.test.ts
@@ -0,0 +1,7 @@
+import shuffle from '../source/shuffle';
+
+test('shuffle([1, 2, 3, 4])', () => {
+    const randomArr = shuffle([1, 2, 3, 4, 5]);
+    expect(randomArr).toEqual(expect.arrayContaining([1, 2, 3, 4, 5]));
+    expect([1, 2, 3, 4, 5]).toEqual(expect.arrayContaining(randomArr));
+});
