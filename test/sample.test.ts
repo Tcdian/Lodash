8c3b487a4856c862b939771d7f85546df25af22d
tcdian <tcdiank@gmail.com>
2020-05-12 21:08:44 +0800

    :sparkles: sample

---
+++ test/sample.test.ts
@@ -0,0 +1,13 @@
+import sample from '../source/sample';
+
+test('sample([1, 2, 3, 4])', () => {
+    const randomEl = sample([1, 2, 3, 4]);
+    expect(randomEl).toBeLessThanOrEqual(4);
+    expect(randomEl).toBeGreaterThanOrEqual(1);
+});
+
+test('sample({ a: 1, b: 2, c: 3, d: 4 })', () => {
+    const randomEl = sample({ a: 1, b: 2, c: 3, d: 4 });
+    expect(randomEl).toBeLessThanOrEqual(4);
+    expect(randomEl).toBeGreaterThanOrEqual(1);
+});
