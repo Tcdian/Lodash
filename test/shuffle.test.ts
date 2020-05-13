ae74473b64dba88adfcceabe3094eb1c05fe46c8
tcdian <tcdiank@gmail.com>
2020-05-13 19:45:16 +0800

    :label: shuffle

--- test/shuffle.test.ts
+++ test/shuffle.test.ts
@@ -1,6 +1,6 @@
 import shuffle from '../source/shuffle';
 
-test('shuffle([1, 2, 3, 4])', () => {
+test('shuffle([1, 2, 3, 4, 5])', () => {
     const randomArr = shuffle([1, 2, 3, 4, 5]);
     expect(randomArr).toEqual(expect.arrayContaining([1, 2, 3, 4, 5]));
     expect([1, 2, 3, 4, 5]).toEqual(expect.arrayContaining(randomArr));
