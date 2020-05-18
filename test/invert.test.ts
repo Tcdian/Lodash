dddb5a360bb9f4b21d86ea60333d38deb2420e57
tcdian <tcdiank@gmail.com>
2020-05-18 21:39:08 +0800

    :sparkles: invert

---
+++ test/invert.test.ts
@@ -0,0 +1,6 @@
+import _ from 'lodash';
+import invert from '../source/invert';
+
+test(`invert({ a: 1, b: 2, c: 1 }) => ${invert({ a: 1, b: 2, c: 1 })}`, () => {
+    expect(invert({ a: 1, b: 2, c: 1 })).toEqual(_.invert({ a: 1, b: 2, c: 1 }));
+});
