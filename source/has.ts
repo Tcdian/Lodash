31d1415bb0a9acb46d42ef0b2fb9e8b24e223fdf
tcdian <tcdiank@gmail.com>
2020-05-18 21:11:29 +0800

    :sparkles: has

--- source/has.ts
+++ source/has.ts
@@ -3,12 +3,17 @@ import toPath from './toPath';
 type PropertyName = string | number | symbol;
 type PropertyPath = PropertyName | ReadonlyArray<PropertyName>;
 
-interface Dictionary<T> {
-    [index: string]: T;
+const objectProto = Object.prototype;
+
+function has(object: any, path: PropertyPath): boolean {
+    const pathArr = toPath(path);
+    for (let i = 0; i < pathArr.length; i++) {
+        if (!objectProto.hasOwnProperty.call(object, pathArr[i])) {
+            return false;
+        }
+        object = object[pathArr[i]];
+    }
+    return true;
 }
 
-interface NumericDictionary<T> {
-    [index: number]: T;
-}
-
-function has(object: Dictionary<any> | NumericDictionary<any>, path: PropertyPath): boolean {}
+export default has;
---
+++ test/has.test.ts
@@ -0,0 +1,22 @@
+import _ from 'lodash';
+import has from '../source/has';
+import create from '../source/create';
+
+const object = { a: { b: 2 } };
+const other = create({ a: create({ b: 2 }) });
+
+test(`has({ a: { b: 2 } }, 'a') => ${has(object, 'a')}`, () => {
+    expect(has(object, 'a')).toBe(_.has(object, 'a'));
+});
+
+test(`has({ a: { b: 2 } }, 'a.b') => ${has(object, 'a.b')}`, () => {
+    expect(has(object, 'a.b')).toBe(_.has(object, 'a.b'));
+});
+
+test(`has({ a: { b: 2 } }, ['a', 'b']) => ${has(object, ['a', 'b'])}`, () => {
+    expect(has(object, ['a', 'b'])).toBe(_.has(object, ['a', 'b']));
+});
+
+test(`has(create({ a: create({ b: 2 }) }), 'a') => ${has(other, 'a')}`, () => {
+    expect(has(other, 'a')).toBe(_.has(other, 'a'));
+});
