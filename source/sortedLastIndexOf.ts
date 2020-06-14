0f39ef6638e669922202d7e241dbf1d7f0e5df08
tcdian <tcdiank@gmail.com>
2020-06-14 17:33:32 +0800

    :art: sortedLastIndexOf

--- source/sortedLastIndexOf.ts
+++ source/sortedLastIndexOf.ts
@@ -2,7 +2,7 @@ import sortedLastIndex from './sortedLastIndex';
 
 function sortedLastIndexOf<T>(array: T[], value: T): number {
     const index = sortedLastIndex(array, value) - 1;
-    return array[index] === value ? index : -1;
+    return index < 0 || array[index] === value ? index : -1;
 }
 
 export default sortedLastIndexOf;
