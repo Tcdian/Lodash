ace12030bb86c825577a8c0dab7fb05d06044cb5
tcdian <tcdiank@gmail.com>
2020-06-14 17:19:51 +0800

    :sparkles: pullAllWith

---
+++ test/pullAllWith.test.ts
@@ -0,0 +1,16 @@
+import _ from 'lodash';
+import pullAllWith from '../source/pullAllWith';
+
+const array = [
+    { x: 1, y: 2 },
+    { x: 3, y: 4 },
+    { x: 5, y: 6 },
+];
+
+test("pullAllWith([{ 'x': 1, 'y': 2 }, { 'x': 3, 'y': 4 }, { 'x': 5, 'y': 6 }], [{ 'x': 3, 'y': 4 }], _.isEqual)", () => {
+    pullAllWith(array, [{ x: 3, y: 4 }], _.isEqual);
+    expect(array).toEqual([
+        { x: 1, y: 2 },
+        { x: 5, y: 6 },
+    ]);
+});
