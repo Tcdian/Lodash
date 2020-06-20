00fd975eb2f96a98601c5e08a75391735a64b655
tcdian <tcdiank@gmail.com>
2020-06-20 17:34:51 +0800

    :sparkles: zipWith

--- source/zip.ts
+++ source/zip.ts
@@ -1,8 +1,8 @@
-function zip<T>(...arrays: T[][]): T[][];
 function zip<T1, T2>(arr1: T1[], arr2: T2[]): [T1, T2][];
 function zip<T1, T2, T3>(arr1: T1[], arr2: T2[], arr3: T3[]): [T1, T2, T3][];
 function zip<T1, T2, T3, T4>(arr1: T1[], arr2: T2[], arr3: T3[], arr4: T4[]): [T1, T2, T3, T4][];
 function zip<T1, T2, T3, T4, T5>(arr1: T1[], arr2: T2[], arr3: T3[], arr4: T4[], arr5: T5[]): [T1, T2, T3, T4, T5][];
+function zip<T>(...arrays: T[][]): T[][];
 function zip<T>(...arrays: T[][]): T[][] {
     const result: T[][] = Array.from(new Array(arrays[0].length), () => new Array(arrays.length));
     for (let i = 0; i < arrays.length; i++) {
---
+++ test/zipWith.test.ts
@@ -0,0 +1,21 @@
+import _ from 'lodash';
+import zipWith from '../source/zipWith';
+
+test(`zipWith([1, 2], [10, 20], [100, 200], function(a, b, c) { return a + b + c; }) => ${zipWith(
+    [1, 2],
+    [10, 20],
+    [100, 200],
+    function (a, b, c) {
+        return a + b + c;
+    }
+)}`, () => {
+    expect(
+        zipWith([1, 2], [10, 20], [100, 200], function (a, b, c) {
+            return a + b + c;
+        })
+    ).toEqual(
+        _.zipWith([1, 2], [10, 20], [100, 200], function (a, b, c) {
+            return a + b + c;
+        })
+    );
+});
