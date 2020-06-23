102e9917d82b447bccea3ec9dc06728aef36833c
tcdian <tcdiank@gmail.com>
2020-06-23 20:45:58 +0800

    :sparkles: assignWith

---
+++ source/assignWith.ts
@@ -0,0 +1,48 @@
+import last from './last';
+import initial from './initial';
+import isFunction from './isFunction';
+import isUndefined from './isUndefined';
+
+type AssignCustomizer<TObject, TSource> = (
+    objValue: TObject[keyof TObject],
+    srcValue: TSource[keyof TSource],
+    key?: keyof TObject,
+    object?: TObject,
+    source?: TSource
+) => any;
+
+function assignWith<TObject, TSource>(
+    object: TObject,
+    source: TSource,
+    customizer: AssignCustomizer<TObject, TSource>
+): TObject & TSource;
+function assignWith<TObject, TSource1, TSource2>(
+    object: TObject,
+    source1: TSource1,
+    source2: TSource2,
+    customizer: AssignCustomizer<TObject, TSource1 & TSource2>
+): TObject & TSource1 & TSource2;
+function assignWith<TObject, TSource1, TSource2, TSource3>(
+    object: TObject,
+    source1: TSource1,
+    source2: TSource2,
+    source3: TSource3,
+    customizer: AssignCustomizer<TObject, TSource1 & TSource2 & TSource3>
+): TObject & TSource1 & TSource2 & TSource3;
+function assignWith(object: any, ...otherArgs: any[]): any;
+function assignWith(object: any, ...otherArgs: any[]): any {
+    const customizer: AssignCustomizer<any, any> = last(otherArgs);
+    if (!isFunction(customizer)) {
+        return Object.assign(object, ...otherArgs);
+    }
+    const sources = initial(otherArgs);
+    sources.forEach((source) => {
+        Object.keys(source).forEach((key) => {
+            const customized = customizer(object[key], source[key], key, object, source);
+            object[key] = isUndefined(customized) ? source[key] : customized;
+        });
+    });
+    return object;
+}
+
+export default assignWith;
---
+++ test/assignWith.test.ts
@@ -0,0 +1,12 @@
+import _ from 'lodash';
+import assignWith from '../source/assignWith';
+
+function customizer(objValue: number | undefined, srcValue: number | undefined) {
+    return _.isUndefined(objValue) ? srcValue : objValue;
+}
+
+test('assignWith', () => {
+    expect(assignWith({ a: 1 }, { b: 2 }, { a: 3 }, customizer)).toEqual(
+        _.assignWith({ a: 1 }, { b: 2 }, { a: 3 }, customizer)
+    );
+});
