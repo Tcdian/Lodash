8ad81fa4ac076f4dd97f90d66fa2a1ba2f6a97da
tcdian <tcdiank@gmail.com>
2020-06-23 21:02:23 +0800

    :sparkles: assignInWith

---
+++ source/assignInWith.ts
@@ -0,0 +1,48 @@
+import last from './last';
+import initial from './initial';
+import isFunction from './isFunction';
+import isUndefined from './isUndefined';
+
+type AssignCustomizer<TObject, TSource> = (
+    objValue: TObject[keyof TObject],
+    srcValue: TSource[keyof TSource],
+    key?: keyof TObject,
+    object?: TObject,
+    source?: TSource
+) => any;
+
+function assignInWith<TObject, TSource>(
+    object: TObject,
+    source: TSource,
+    customizer: AssignCustomizer<TObject, TSource>
+): TObject & TSource;
+function assignInWith<TObject, TSource1, TSource2>(
+    object: TObject,
+    source1: TSource1,
+    source2: TSource2,
+    customizer: AssignCustomizer<TObject, TSource1 & TSource2>
+): TObject & TSource1 & TSource2;
+function assignInWith<TObject, TSource1, TSource2, TSource3>(
+    object: TObject,
+    source1: TSource1,
+    source2: TSource2,
+    source3: TSource3,
+    customizer: AssignCustomizer<TObject, TSource1 & TSource2 & TSource3>
+): TObject & TSource1 & TSource2 & TSource3;
+function assignInWith(object: any, ...otherArgs: any[]): any;
+function assignInWith(object: any, ...otherArgs: any[]): any {
+    const customizer: AssignCustomizer<any, any> = last(otherArgs);
+    if (!isFunction(customizer)) {
+        return Object.assign(object, ...otherArgs);
+    }
+    const sources = initial(otherArgs);
+    sources.forEach((source) => {
+        for (let key in source) {
+            const customized = customizer(object[key], source[key], key, object, source);
+            object[key] = isUndefined(customized) ? source[key] : customized;
+        }
+    });
+    return object;
+}
+
+export default assignInWith;
---
+++ test/assignInWith.test.ts
@@ -0,0 +1,33 @@
+import _ from 'lodash';
+import create from '../source/create';
+import assignInWith from '../source/assignInWith';
+
+let source1: { a: number };
+let source2: { b: number };
+
+beforeEach(() => {
+    source1 = create({ a: 3 });
+    source2 = create({ b: 3 });
+});
+
+function customizer(objValue: number | undefined, srcValue: number | undefined) {
+    return _.isUndefined(objValue) ? srcValue : objValue;
+}
+
+test('assignInWith({ a: 1 }, { b: 2 }, { a: 3 }, customizer)', () => {
+    expect(assignInWith({ a: 1 }, { b: 2 }, { a: 3 }, customizer)).toEqual(
+        _.assignInWith({ a: 1 }, { b: 2 }, { a: 3 }, customizer)
+    );
+});
+
+test('assignInWith({ a: 1 }, { b: 2 }, source1, customizer)', () => {
+    expect(assignInWith({ a: 1 }, { b: 2 }, source1, customizer)).toEqual(
+        _.assignInWith({ a: 1 }, { b: 2 }, source1, customizer)
+    );
+});
+
+test('assignInWith({ a: 1 }, { b: 2 }, source1, source2, customizer)', () => {
+    expect(assignInWith({ a: 1 }, { b: 2 }, source1, source2, customizer)).toEqual(
+        _.assignInWith({ a: 1 }, { b: 2 }, source1, source2, customizer)
+    );
+});
