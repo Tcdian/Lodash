5511edaa718c608277694511c5b0bfc94522063a
tcdian <tcdiank@gmail.com>
2020-06-23 21:03:34 +0800

    ✅ update assignIn test

--- test/assignIn.test.ts
+++ test/assignIn.test.ts
@@ -2,22 +2,22 @@ import _ from 'lodash';
 import create from '../source/create';
 import assignIn from '../source/assignIn';
 
-let foo: { a: number; b: number };
-let bar: { c: number; d: number };
+let source1: { a: number; b: number };
+let source2: { c: number; d: number };
 
 beforeEach(() => {
-    foo = create({ b: 2 }, { a: 1 });
-    bar = create({ d: 4 }, { c: 3 });
+    source1 = create({ b: 2 }, { a: 1 });
+    source2 = create({ d: 4 }, { c: 3 });
 });
 
 test('assignIn({ a: 0 }, create({ b: 2 }, { a: 1 }))', () => {
-    expect(assignIn({ a: 0 }, foo)).toEqual(_.assignIn({ a: 0 }, foo));
+    expect(assignIn({ a: 0 }, source1)).toEqual(_.assignIn({ a: 0 }, source1));
 });
 
 test('assignIn({ a: 0 }, create({ d: 4 }, { c: 3 }))', () => {
-    expect(assignIn({ a: 0 }, bar)).toEqual(_.assignIn({ a: 0 }, bar));
+    expect(assignIn({ a: 0 }, source2)).toEqual(_.assignIn({ a: 0 }, source2));
 });
 
 test('assignIn({ a: 0 }, create({ b: 2 }, { a: 1 }), create({ d: 4 }, { c: 3 }))', () => {
-    expect(assignIn({ a: 0 }, foo, bar)).toEqual(_.assignIn({ a: 0 }, foo, bar));
+    expect(assignIn({ a: 0 }, source1, source2)).toEqual(_.assignIn({ a: 0 }, source1, source2));
 });
