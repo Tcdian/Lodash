1e7e0a081a8a75056f041d74053955e712e82be8
tcdian <tcdiank@gmail.com>
2021-06-04 11:14:40 +0800

    ✏️ fix overEvery overSome types

--- source/util/overEvery.ts
+++ source/util/overEvery.ts
@@ -1,7 +1,7 @@
 import { identity } from './identity';
 
 function overEvery<TArgs extends any[]>(predicates: ((...args: TArgs) => boolean)[] = [identity as any]) {
-    return function (this: any, ...args: TArgs) {
+    return function (this: any, ...args: TArgs): boolean {
         return predicates.every((predicate) => predicate.call(this, ...args));
     };
 }
--- source/util/overSome.ts
+++ source/util/overSome.ts
@@ -1,7 +1,7 @@
 import { identity } from './identity';
 
 function overSome<TArgs extends any[]>(predicates: ((...args: TArgs) => boolean)[] = [identity as any]) {
-    return function (this: any, ...args: TArgs) {
+    return function (this: any, ...args: TArgs): boolean {
         return predicates.some((predicate) => predicate.call(this, ...args));
     };
 }
