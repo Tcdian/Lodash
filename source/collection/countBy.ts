293ce837eb7397e960759f54e199564b7e943c68
tcdian <tcdiank@gmail.com>
2021-06-09 15:49:49 +0800

    ✨ countBy

--- source/collection/countBy.ts
+++ source/collection/countBy.ts
@@ -1,1 +1,40 @@
-// todo ...
+import { identity } from '../util/identity';
+import { iteratee } from '../util/iteratee';
+import { values } from '../object/values';
+import { assign } from '../object/assign';
+
+type PropertyName = string | number | symbol;
+type IterateeShorthand<T> = PropertyName | [PropertyName, any] | Partial<T>;
+type ValueIterator<T, TResult> = (value: T) => TResult;
+type ValueIterateeCustom<T, TResult> = ValueIterator<T, TResult> | IterateeShorthand<T>;
+
+function countBy<T, TResult extends PropertyName>(
+    collection: T[],
+    predicate?: ValueIterateeCustom<T, TResult>
+): Record<TResult, number>;
+function countBy<TResult extends PropertyName>(
+    collection: string,
+    predicate?: ValueIterateeCustom<string, TResult>
+): Record<TResult, number>;
+function countBy<K extends PropertyName, V, TResult extends PropertyName>(
+    collection: Record<K, V>,
+    predicate?: ValueIterateeCustom<V, TResult>
+): Record<TResult, number>;
+function countBy<TResult extends PropertyName>(
+    collection: any,
+    predicate: ValueIterateeCustom<any, TResult> = identity
+): Record<TResult, number> {
+    const result: Record<PropertyName, number> = {};
+    const iterateeFunc = iteratee(predicate);
+    values(collection).forEach((value) => {
+        const generated = iterateeFunc(value);
+        if (Object.prototype.hasOwnProperty.call(result, generated)) {
+            result[generated] += 1;
+        } else {
+            assign(result, { [generated]: 1 });
+        }
+    });
+    return result;
+}
+
+export { countBy };
---
+++ test/countBy.test.ts
@@ -0,0 +1,11 @@
+import { countBy } from '../source/collection/countBy';
+
+describe('countBy', () => {
+    test('countBy iterator', () => {
+        expect(countBy([6.1, 4.2, 6.3], Math.floor)).toEqual({ '4': 1, '6': 2 });
+    });
+
+    test('The "property" iteratee shorthand.', () => {
+        expect(countBy(['one', 'two', 'three'], 'length')).toEqual({ '3': 2, '5': 1 });
+    });
+});
