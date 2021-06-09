6d4ea6c29d3f8531ee482cdb15c4d2f139de79b7
tcdian <tcdiank@gmail.com>
2021-06-09 13:33:24 +0800

    ✨ takeWhile

---
+++ test/takeWhile.test.ts
@@ -0,0 +1,31 @@
+import { takeWhile } from '../source/array/takeWhile';
+
+describe('takeWhile', () => {
+    const users = [
+        { user: 'barney', active: false },
+        { user: 'fred', active: false },
+        { user: 'pebbles', active: true },
+    ];
+
+    test('dropWhile iterator', () => {
+        expect(takeWhile(users, (user) => !user.active)).toEqual([
+            { user: 'barney', active: false },
+            { user: 'fred', active: false },
+        ]);
+    });
+
+    test('The "matches" iteratee shorthand.', () => {
+        expect(takeWhile(users, { user: 'barney', active: false })).toEqual([{ user: 'barney', active: false }]);
+    });
+
+    test('The "matchesProperty" iteratee shorthand.', () => {
+        expect(takeWhile(users, ['active', false])).toEqual([
+            { user: 'barney', active: false },
+            { user: 'fred', active: false },
+        ]);
+    });
+
+    test('The "property" iteratee shorthand.', () => {
+        expect(takeWhile(users, 'active')).toEqual([]);
+    });
+});
