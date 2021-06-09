81161ae81fbe00d3ef088aed65d7fa31a7edaf17
tcdian <tcdiank@gmail.com>
2021-06-09 10:23:05 +0800

    ✨ dropWhile

---
+++ test/dropWhile.test.ts
@@ -0,0 +1,32 @@
+import { dropWhile } from '../source/array/dropWhile';
+
+describe('dropWhile', () => {
+    const users = [
+        { user: 'barney', active: false },
+        { user: 'fred', active: false },
+        { user: 'pebbles', active: true },
+    ];
+
+    test('dropWhile iterator', () => {
+        expect(dropWhile(users, (user) => !user.active)).toEqual([{ user: 'pebbles', active: true }]);
+    });
+
+    test('The "matches" iteratee shorthand.', () => {
+        expect(dropWhile(users, { user: 'barney', active: false })).toEqual([
+            { user: 'fred', active: false },
+            { user: 'pebbles', active: true },
+        ]);
+    });
+
+    test('The "matchesProperty" iteratee shorthand.', () => {
+        expect(dropWhile(users, ['active', false])).toEqual([{ user: 'pebbles', active: true }]);
+    });
+
+    test('The "property" iteratee shorthand.', () => {
+        expect(dropWhile(users, 'active')).toEqual([
+            { user: 'barney', active: false },
+            { user: 'fred', active: false },
+            { user: 'pebbles', active: true },
+        ]);
+    });
+});
